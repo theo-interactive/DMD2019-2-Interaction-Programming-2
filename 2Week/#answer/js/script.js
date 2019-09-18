0eafe59e00300fe105b67049fa71c757d7162a23
rh <>
2019-09-18 17:51:48 +0900

    2주차 문제 풀이 준비.

--- 2Week/#answer/js/script.js
+++ 2Week/#answer/js/script.js
@@ -1,11 +1,7 @@
-# 2 WEEK
+var logStyle = "padding:2px;background:#fff;border-radius:4px;color:#222;";
 
-2주차 과제입니다.
-
-'학번_이름' 폴더 생성 후 index.html 파일 만들어서 폴더 업로드합니다.
-
-### Quest 1.
-
+//Answer 1.
+/*
 1. selectors 라는 이름을 가진 객체를 생성합니다.
 2. number 프로퍼티를 갖고 있으며 그 값은 2 (number type) 입니다.
 3. string 프로퍼티를 추가하고 빈 문자열 ‘ ’ 을 할당합니다.
@@ -13,60 +9,38 @@
 5. selectors 객체의 dom 배열 프로퍼티에 ‘main’, ‘footer’ 라는 문자열을 ‘header’ 원소의 뒤에 순서대로 추가합니다.
 6. selectors 객체의 string 프로퍼티를 삭제합니다.
 7. console.log 메서드를 이용해서 selectors 객체를 출력합니다.
+*/
+console.log("%c Answer 1. ", logStyle);
 
-### Quest 2.
 
+//Answer 2.
+/*
 1. var cars = [‘Hyundai’, ‘Kia’, ‘Samsung’, ‘GM’]; 배열을 선언합니다.
 2. cars 배열 문자열 원소를 역순으로 출력하는 반복문을 작성합니다.
 3. console.log 메서드를 이용해서 cars 를 출력합니다.
+*/
+console.log("%c Answer 2. ", logStyle);
 
-### Quest 3.
 
+//Answer 3.
+/*
 1. 전역변수 document 의 프로퍼티 및 메서드 등을 console.log 메서드를 이용해서 출력하는 반복문을 작성합니다.
 2. 여러가지 방법의 반복문을 이용하여 작성해도 됩니다.
+*/
+console.log("%c Answer 3. ", logStyle);
 
-### Quest 4.
-
+//Answer 4.
+/*
 1. var orders = { name : [‘본인 이름’], coffee : [‘아메리카노’, ‘에스프레소’, ‘카페라떼’] }; 객체를 선언합니다.
 2. orders 객체의 coffee 프로퍼티 배열이 가지는 원소들을 반복문을 이용하여 (3) 의 문자열로 console.log 메서드를 이용해서 출력합니다.
 3. 문자열 : “name 님, 주문하신 coffee 1잔 나왔습니다.”
+*/
+console.log("%c Answer 4. ", logStyle);
 
-### Quest 5.
-
-1. 객체, 배열, 반복문, 함수 등을 이용하여 자기 소개를 하는 문자열을 출력합니다.
-2. 자신의 소개를 해주세요.
-
-## 제출기한
-
-9월 11일 (수) 23:00 까지 업로드
-
-## index.html 형식
-
-```
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <title>2WEEK</title>
-</head>
-<body>
-<h1>2WEEK - 09/05</h1>
-<h2>학번 / 이름</h2>
-<script src="./js/script.js" type="text/javascript"></script>
-</body>
-</html>
-```
-
-## ./js/script.js 형식
-
-```
-//Answer 1.
-
-//Answer 2.
-
-//Answer 3.
-
-//Answer 4.
 
 //Answer 5.
-```
+/*
+1. 객체, 배열, 반복문, 함수 등을 이용하여 자기 소개를 하는 문자열을 출력합니다.
+2. 자신의 소개를 해주세요.
+*/
+console.log("%c Answer 5. ", logStyle);
