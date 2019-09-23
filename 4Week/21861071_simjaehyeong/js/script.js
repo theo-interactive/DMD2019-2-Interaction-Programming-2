dadfb7f7bb12c6dc55e775dfe19dab34877715d7
심재형 <eowk3130@naver.com>
2019-09-23 16:58:05 +0900

    4주차 / 21861071 심재형 과제제출

---
+++ 4Week/21861071_simjaehyeong/js/script.js
@@ -0,0 +1,56 @@
+var logStyle = "padding:2px;background:#fff;border-radius:4px;color:#222;";
+(function($){
+    $(document).ready(function(){
+        //Quest 1.
+        /*
+        모든 li 요소들을 선택하고 addClass 메서드를 이용하여 active 클래스를 추가합니다.
+        */
+        //Answer 1.
+        // console.log($('li'));
+        $('li').addClass('active');
+
+        //Quest 2.
+        /*
+        문서내의 a 요소이면서 type-2 클래스를 가진 노드를 선택하고, 해당 요소에 current 클래스를 추가합니다.
+        */
+        //Answer 2.
+        // console.log($('a.type-2'));
+        $('a.type-2').addClass('current');
+
+        //Quest 3.
+        /*
+        문서내의 a 요소이면서 URL 링크 속성값이 있는 노드를 선택하고, 해당 요소에 underline 클래스를 추가합니다.
+        */
+        //Answer 3.
+        // console.log($('a[href^="http://"]')); // URL 링크 속성값은 "http://~~"" 이니 공통으로 포함하는 부분인 "http://"으로 찾는다.
+        $('a[href^="http://"]').addClass('underline');   //이건 "http://"를 포함했다면 불러온다.
+        // $('a[href="http://"]').addClass('underline'); //이건  아예 "http://"만 있는 친구만 불러온다.
+        
+
+        //Quest 4.
+        /*
+        문서내의 li 요소이면서 data-role 속성이 list 인 노드를 선택하고, 해당 요소에 role 클래스를 추가합니다.
+        */
+        //Answer 4.
+        // console.log($('li'))
+        $('li[data-role="list"]').addClass('role');
+
+        //Quest 5.
+        /*
+        menu 아이디를 가진 nav 요소를 선택하고, open 클래스를 삭제합니다.
+        */
+        //Answer 5.
+        // console.log($('nav#manu'));
+        $('nav#menu').removeClass('open');
+
+        //Quest 6.
+        /*
+        content 클래스를 가진 section 요소 노드들의 길이를 구하고 console.log 메서드를 이용하여 출력합니다.
+        */
+        //Answer 6.
+        console.log("%c Answer 6.", logStyle);
+        var length = $('section.content').length;
+        console.log(length);
+        
+    });
+})(jQuery);
