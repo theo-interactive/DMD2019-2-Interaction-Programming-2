8b62fe7c7a57e40e94bd35815e6822f9f8b77488
백지연 <joy095123@gmail.com>
2019-09-25 13:41:08 +0900

    commit
    2176151_백지연

--- 4Week/21761051_Baekjiyeon/js/script.js
+++ 4Week/21761051_Baekjiyeon/js/script.js
@@ -6,6 +6,7 @@ var logStyle = "padding:2px;background:#fff;border-radius:4px;color:#222;";
         모든 li 요소들을 선택하고 addClass 메서드를 이용하여 active 클래스를 추가합니다.
         */
         //Answer 1.
+        $('li').addClass('active');
 
 
         //Quest 2.
@@ -13,6 +14,7 @@ var logStyle = "padding:2px;background:#fff;border-radius:4px;color:#222;";
         문서내의 a 요소이면서 type-2 클래스를 가진 노드를 선택하고, 해당 요소에 current 클래스를 추가합니다.
         */
         //Answer 2.
+        $('a.type-2').addClass('current');
 
 
         //Quest 3.
@@ -20,6 +22,7 @@ var logStyle = "padding:2px;background:#fff;border-radius:4px;color:#222;";
         문서내의 a 요소이면서 URL 링크 속성값이 있는 노드를 선택하고, 해당 요소에 underline 클래스를 추가합니다.
         */
         //Answer 3.
+        $('a[href="#"]').addClass('underline');
 
 
         //Quest 4.
@@ -27,6 +30,7 @@ var logStyle = "padding:2px;background:#fff;border-radius:4px;color:#222;";
         문서내의 li 요소이면서 data-role 속성이 list 인 노드를 선택하고, 해당 요소에 role 클래스를 추가합니다.
         */
         //Answer 4.
+        $('li[data-role=list]').addClass('role');
 
 
         //Quest 5.
@@ -34,6 +38,7 @@ var logStyle = "padding:2px;background:#fff;border-radius:4px;color:#222;";
         menu 아이디를 가진 nav 요소를 선택하고, open 클래스를 삭제합니다.
         */
         //Answer 5.
+        $('nav#menu').removeClass('open');
 
 
         //Quest 6.
@@ -42,6 +47,8 @@ var logStyle = "padding:2px;background:#fff;border-radius:4px;color:#222;";
         */
         //Answer 6.
         console.log("%c Answer 6.", logStyle);
+        $('section.content').length;
+
         
     });
 })(jQuery);
