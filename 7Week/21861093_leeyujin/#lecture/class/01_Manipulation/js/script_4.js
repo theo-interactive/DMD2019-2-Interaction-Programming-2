335662bb6cbcf7e3dcdec8671f2fe99d8eeadb03
bobobbo <48302947+bobobbo@users.noreply.github.com>
2019-10-16 22:29:17 +0900

    21861093_이유진7주차

---
+++ 7Week/21861093_leeyujin/#lecture/class/01_Manipulation/js/script_4.js
@@ -0,0 +1,22 @@
+(function($) {
+    $(document).ready(function() {
+         //$('p').eq(0).detach();
+        // 해당 노드를 삭제.
+
+        //$('p').eq(2).empty();
+        // 해단 노드의 html 내용 비우기.
+
+        $('.btn-3').click(function(e) {
+            e.preventDefault();
+            //$('p').last().remove();
+            //$(this).next('p').remove();
+            if($(this).next('p') !== 'undefined'){
+                $(this).next('p').detach();
+            }
+            // console.log($(this).next());
+        });
+
+        $('p').eq(1).wrap('<div><div></div></div>');
+        $('p').eq(1).unwrap();
+    });
+})(jQuery);
