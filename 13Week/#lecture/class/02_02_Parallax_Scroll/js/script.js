82ba889dd69c25a08eb5dd8c0b1454db31308d58
rh <>
2019-11-21 02:15:10 +0900

    13주차 수업 준비.

---
+++ 13Week/#lecture/class/02_02_Parallax_Scroll/js/script.js
@@ -0,0 +1,18 @@
+(function($){
+    $(document).ready(function(){
+        var Parallax = {
+            init: function() {
+                this.layout();
+                this.reset();
+                this.addEvent();
+            },
+            layout: function() {
+            },
+            reset: function() {
+            },
+            addEvent: function() {
+            }
+        }
+        Parallax.init();
+    });
+})(jQuery);
